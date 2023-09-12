0ed7c032a7c90ce09a33afb5233a930d834f9323
petite_marie <minaeva.mav@gmail.com>
2023-09-12 16:56:15 +0200

    subset DS6 to 1 cell line

--- main_app.py
+++ main_app.py
@@ -5,13 +5,22 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from time import time
+from tqdm import tqdm
 
 start = time()
-sim = SergioSimulator(number_genes=100, number_bins=1, number_sc=300, bio_noise_params=1, decays=0.8, sampling_state=15, noise_type='dpd')
-sim.generate_data(input_file_targets = "./steady-state_input_GRN.txt",
-                    input_file_regs = "./steady-state_input_MRs.txt",
-                    filename="try_mth", 
-                    add_noise=False)
+for prob in tqdm(np.linspace(0.0, 6.0, 13)):
+    sim = SergioSimulator(number_genes=1200, number_bins=1, number_sc=300, bio_noise_params=1, decays=0.8, sampling_state=15, noise_type='dpd')
+    sim.generate_data(input_file_targets = "SERGIO/Interaction_cID_6.txt",
+                        input_file_regs = "SERGIO/Regs_cID_6.txt",
+                        filename="cID_6_outlier_mean_" + str(np.round(prob, 2)), 
+                        add_noise=True,
+                    noise_params={"outlier_prob": 0.01,
+                                  "outlier_mean": prob,
+                                  "outlier_scale": 1.0,
+                                  "lib_mean": 4.5,
+                                  "lib_scale": 0.7,
+                                  "shape": 8,
+                                  "percentile": 45.0})
 end = time()
 
 print(f"Finished in {end - start}")
